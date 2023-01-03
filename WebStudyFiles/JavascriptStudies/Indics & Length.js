a6713de0b8cc363631db51a52964392c247daabe
Rockpushedseaweed <102870721+Rockpushedseaweed@users.noreply.github.com>
2023-01-03 21:41:37 +0900

    Update Indics & Length.js

--- WebStudyFiles/JavascriptStudies/Indics & Length.js
+++ WebStudyFiles/JavascriptStudies/Indics & Length.js
@@ -1,1 +1,20 @@
-﻿
+﻿/*
+ * let animal = "Dumbo Octopus";
+ * animal [0]
+ * D
+ * animal[6]
+ * O
+ * animal[99]
+ * undefined
+ *
+ * aniaml.length
+ * 12
+ *
+ * "lol"+"lol"
+ * "lollol"
+ *
+ * 문자열은 바꿀 수 없음.
+ *
+ * 1 + "hi"
+ * "1hi"
+ *  
