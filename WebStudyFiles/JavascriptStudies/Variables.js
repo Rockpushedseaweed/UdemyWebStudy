9e69f1b372104aa486a8226b64207f73a2b3feb6
Rockpushedseaweed <102870721+Rockpushedseaweed@users.noreply.github.com>
2023-01-03 21:02:44 +0900

    01.03

---
+++ WebStudyFiles/JavascriptStudies/Variables.js
@@ -0,0 +1,22 @@
+﻿/*
+ * Variables 변수
+ *
+ * 숫자 변수
+ * let year = 1997;
+ * recall -> year -> 1997
+ *
+ * const와 let의 차이점
+ * const 원주율과 같이 고정된 상수값에 지정. (변하면 안되는 값) 재할당 불가능 .
+ * let 값이 변할 수 있음. 재할당 가능
+ *
+ * Booleans: true or false(소문자)
+ * 만약 다른 값으로 지정하면 다른 변수타입으로 변경될 수 있음 .
+ *
+ *
+ * JS Coding Rules
+ * 변수 숫자로 시작 불가.
+ * _는 사용가능
+ * Camel case:  applePie
+ * Snake case:  apple_pie
+ * 변수명은 최대한 알아볼 수 있도록 친절하면서 명료하게 지정
+ * 
