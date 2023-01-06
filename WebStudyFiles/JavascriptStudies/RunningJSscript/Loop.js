a435c025bb46e7f00c3a482d0b3cbfd7f3c40ba5
Rockpushedseaweed <102870721+Rockpushedseaweed@users.noreply.github.com>
2023-01-06 18:16:01 +0900

    01.06

---
+++ WebStudyFiles/JavascriptStudies/RunningJSscript/Loop.js
@@ -0,0 +1,16 @@
+Loop
+
+/*
+
+*For 
+
+for (현재값; 조건식; 참이면--해라) 
+for(
+    [initialExpression];
+    [condition];
+    [incrementExpression]
+){}
+
+for(let i = 1; i<=10; i++){
+    console.log(i)
+}
