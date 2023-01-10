9a13833eb0eb3c720db3a191c20165a6d800d51c
Rockpushedseaweed <102870721+Rockpushedseaweed@users.noreply.github.com>
2023-01-10 16:12:58 +0900

    01.10

--- WebStudyFiles/HTMLStudies/JavascriptStudies/function.js
+++ WebStudyFiles/HTMLStudies/JavascriptStudies/function.js
@@ -14,3 +14,22 @@ function singSong(){
     console.log("Re")
     console.log("MI")
 }
+
+*인수: 함수 결과에 영향을 주는 input들
+
+// function 함수이름(매개변수) {
+//     액션
+// }
+
+function greet(firstName){
+    console.log(`first name is: ${firstName}`)
+}
+
+**여러가지 매개변수
+function repeat(str, numTimes){
+    let result = '';
+    for(let i = 0; i< numTimes; i++){
+        result += str;
+    }
+    console.log(result);
+}
