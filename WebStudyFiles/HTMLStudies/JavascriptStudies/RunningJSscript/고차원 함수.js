99aa4b8e0b180660e2e6e14656fc5dc3bc972955
Rockpushedseaweed <102870721+Rockpushedseaweed@users.noreply.github.com>
2023-01-13 16:37:58 +0900

    01.13

---
+++ WebStudyFiles/HTMLStudies/JavascriptStudies/RunningJSscript/고차원 함수.js
@@ -0,0 +1,22 @@
+function collecEggs(){
+    let total = 0;
+
+}
+
+let radius - 8;
+//if 문 블록임
+if(radius > 0) {
+    const PI = 3.141592;
+    let msg = 'HIII!'
+} 
+
+/** 메소드 만들기 */
+const myMath = {
+    PI: 3.141592,
+    square: function (num){
+        return num * num;
+    },
+    cube: function (num){
+        return num ** 3
+    }
+}
